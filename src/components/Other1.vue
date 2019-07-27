d9d22c06aaf0817da1cba732c2ec3ca0ee0b53aa
mitena <mitenachian@gmail.com>
2019-07-27 09:50:30 +0800

    ++

--- src/components/Other1.vue
+++ src/components/Other1.vue
@@ -1,5 +1,15 @@
 <template>
   <div>
-    Route 3-1 component
+    <span>暫放</span>
+    <br>
+    <a href="http://designlab.tw/" target="_blank">DesignLab</a>
+    <br>
+    <a href="http://mitenahandmade.byethost3.com//" target="_blank">Mitena Hand made</a>
+    http://www.globalnetauto.com/
+    http://www.globalnetauto.com.tw/gna_admin/login.php
+    http://www.globalnetauto.url.tw/gna_helper/login.php
+    https://vaq2.com/admin/gnaadmin/index.php
+    http://www.globalnetauto.url.tw/admin/gnaadmin/index.php
+    https://vaq2.com
   </div>
 </template>
