9eed9d95915dd007dafcce3c732821c228b76512
mitena <mitenachian@gmail.com>
2020-03-20 16:14:43 +0800

    ..

--- vue.config.js
+++ vue.config.js
@@ -2,7 +2,7 @@
 //     publicPath: '/vue-router-elementUI/'
 // }
 module.exports = {
-  baseUrl: './'
+  publicPath: '/vue-mitena-demo/'
 }
 // module.exports = {
 //     publicPath: process.env.NODE_ENV === 'production'
