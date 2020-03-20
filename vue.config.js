f74ca8f200d33f7a6fe369604946c7dc60eec71f
mitena <mitenachian@gmail.com>
2020-03-20 16:01:55 +0800

    ..

--- vue.config.js
+++ vue.config.js
@@ -1,8 +1,8 @@
-// module.exports = {
-//     publicPath: './'
-// }
 module.exports = {
-    publicPath: process.env.NODE_ENV === 'production'
-      ? '/vue-router-elementUI/'
-      : '/'
-  }
+    publicPath: '/vue-router-elementUI/'
+}
+// module.exports = {
+//     publicPath: process.env.NODE_ENV === 'production'
+//       ? '/vue-router-elementUI/'
+//       : '/'
+//   }
